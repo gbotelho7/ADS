740f7d9e96a0e5201accd2a0ea31faa3e030f0df
jplemos5 <jpsfa3@iscte-iul.pt>
2023-11-23 23:17:49 +0000

    Updates
    - Adição leitura de salas
    - Funcionamento correto da leitura de salas
    - Mudado a sequência de ações
    - Adicionado switch para escolher entre File e Url
    - Pequena organização do UI

--- schedule/Scripts/functions.js
+++ schedule/Scripts/functions.js
@@ -1,44 +1,77 @@
-function handleParsedData(results, index, csvDataDiv) {
-    const headersMatch = expectedHeadersArray.every((header) => results.meta.fields.includes(header.trim()));
-    console.log(expectedHeadersArray)
-    console.log(results.meta.fields)
 
-    if (headersMatch) {
-      const table = document.createElement("div");
-      table.setAttribute("id", "csvTable" + index);
-      csvDataDiv.appendChild(table);
+function handleParsedData(results, index, e, csvDataDiv, hiddenDiv, classroomsInput) {
+  let headersMatch;
+  if(classroomsInput){
+    const dataArray = [
+      'Edifício', 'Nome sala', 'Capacidade Normal', 'Capacidade Exame', 'Nº características',
+      'Anfiteatro aulas', 'Apoio técnico eventos', 'Arq 1', 'Arq 2', 'Arq 3', 'Arq 4', 'Arq 5',
+      'Arq 6', 'Arq 9', 'BYOD (Bring Your Own Device)', 'Focus Group', 'Horário sala visível portal público',
+      'Laboratório de Arquitectura de Computadores I', 'Laboratório de Arquitectura de Computadores II',
+      'Laboratório de Bases de Engenharia', 'Laboratório de Electrónica', 'Laboratório de Informática',
+      'Laboratório de Jornalismo', 'Laboratório de Redes de Computadores I', 'Laboratório de Redes de Computadores II',
+      'Laboratório de Telecomunicações', 'Sala Aulas Mestrado', 'Sala Aulas Mestrado Plus', 'Sala NEE',
+      'Sala Provas', 'Sala Reunião', 'Sala de Arquitectura', 'Sala de Aulas normal', 'videoconferencia', 'Átrio'
+    ];
+    headersMatch = dataArray.every((header) => results.meta.fields.includes(header.trim()));
+  }
+  else{
+    headersMatch = expectedHeadersArray.every((header) => results.meta.fields.includes(header.trim()));
+  }
+  console.log(expectedHeadersArray)
+  console.log(results.meta.fields)
 
-      new Tabulator("#csvTable" + index, {
-        data: results.data,
-        layout: "fitData",
-        autoColumns: true,
-        pagination: "local",
-        paginationSize: 7,
-      });
-    } else {
-      alert("As configurações do Ficheiro não correspondem aos esperados. Insira as alterações necessárias. A tabela anterior será eliminada");
+  if (headersMatch) {
+    if(hiddenDiv.style.display === "none"){
+      hiddenDiv.style.display = "block"
     }
+    const table = document.createElement("div");
+    table.setAttribute("id", "csvTable" + index);
+    csvDataDiv.appendChild(table);
+
+    new Tabulator("#csvTable" + index, {
+      data: results.data,
+      layout: "fitData",
+      autoColumns: true,
+      pagination: "local",
+      paginationSize: 7,
+    });
+  } else {
+    if(hiddenDiv.style.display === "block" && classroomsInput){
+      hiddenDiv.style.display = "none"
+    }
+    e.target.value = "";
+    alert("As configurações do Ficheiro não correspondem aos esperados. Insira as alterações necessárias. A tabela anterior será eliminada");
+  }
 };
 
-function parseURLs(urls, csvDataDiv) {
-    csvDataDiv.innerHTML = ""; // Clear previous data
+function parseURLs(urls, e, csvDataDiv, hiddenDiv) {
+  csvDataDiv.innerHTML = ""; // Clear previous data
 
-    for (let i = 0; i < urls.length; i++) {
-        const url = urls[i];
-        //e.target.value = "";
-        Papa.parse(url, {
-        download: true,
-        delimiter: csvSeparator,
-        header: true,
-        complete: function (results) {
-            handleParsedData(results, i, csvDataDiv);
-        }
-        });
-    }
-    saveSettings(csvSeparator, expectedHeaders, dynamicCriterium, hourFormat, dateFormat)
+  for (let i = 0; i < urls.length; i++) {
+      const url = urls[i];
+      Papa.parse(url, {
+      download: true,
+      delimiter: csvSeparator,
+      header: true,
+      complete: function (results) {
+          handleParsedData(results, i, e, csvDataDiv, hiddenDiv, false);
+      }
+    });
+  }
+  saveSettings(csvSeparator, expectedHeaders, dynamicCriterium, hourFormat, dateFormat)
 };
 
 function saveSettings(csvSeparator, expectedHeaders, dynamicCriterium, hourFormat, dateFormat){
-let settings = { "csvSeparator": csvSeparator, "expectedHeaders": expectedHeaders, "dynamicCriterium": dynamicCriterium, "hourFormat": hourFormat, "dateFormat": dateFormat};
-localStorage.setItem('executionData', JSON.stringify(settings)); 
+  let settings = { "csvSeparator": csvSeparator, "expectedHeaders": expectedHeaders, "dynamicCriterium": dynamicCriterium, "hourFormat": hourFormat, "dateFormat": dateFormat};
+  localStorage.setItem('executionData', JSON.stringify(settings)); 
+}
+
+function handleSwitch(csvFileInput, csvUrlInput) {
+  if(toggleSwitch.checked){
+    csvFileInput.style.display = "none"
+    csvUrlInput.style.display = "block"
+  } else{
+    csvFileInput.style.display = "block"
+    csvUrlInput.style.display = "none"
+  }
 }
